c3fa03aa8c0cd56f03fc299514cc39fcb336d726
caro5984 <eaa24csri@students.eaaa.dk>
2025-05-15 22:40:45 +0200

    ændringer i luteal

--- js/luteal.js
+++ js/luteal.js
@@ -28,6 +28,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  let currentIndex = 0;
+  const images = document.querySelectorAll('.tekst-billeder img');
+
+  function nextImage() {
+    // Fjern 'active' fra nuværende billede
+    images[currentIndex].classList.remove('active');
+    // Gå til næste billede (loop tilbage til 0 hvis sidst)
+    currentIndex = (currentIndex + 1) % images.length;
+    // Tilføj 'active' til det nye billede
+    images[currentIndex].classList.add('active');
+  }
+
+
   function checkAnswer(button, isCorrect) {
     const questionBox = button.parentElement;
     const allButtons = questionBox.querySelectorAll('button');
