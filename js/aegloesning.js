8d93ab660ea4debb3e8b91658c4d03abfaca95c5
majabaekgaard <eaa24mcba@students.eaaa.dk>
2025-05-22 17:58:35 +0200

    henvisninger indsat i kommentar

--- js/aegloesning.js
+++ js/aegloesning.js
@@ -1,4 +1,4 @@
-
+//Her får vi hjælp fra ChatGPT: https://chatgpt.com/share/682f166c-a5cc-8010-ab36-8fa46eaed141
 // Vælger hvilke sommerfugle der styles på siden, og tilføjer animation til dem))
 document.addEventListener("DOMContentLoaded", () => {
   const sommerfugle = document.querySelectorAll(".figur1,.figur3,.figur5,.figur7,.figur9");
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
- //js til quiz
+ //js til quiz - Vi har fået hjælp fra ChatGPT her: https://chatgpt.com/share/68272222-0c44-8003-a104-c076eb3a12c0
 function checkAnswer(button, isCorrect) {
   const questionBox = button.parentElement;
   const allButtons = questionBox.querySelectorAll('button');
